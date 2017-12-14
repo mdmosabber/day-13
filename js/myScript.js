2ba438ffa3615c1ff0efa7d34573ff1cc0f66566
mdmosabber <trustcodernet@gmail.com>
2017-12-14 13:14:43 +0600

    Upload class day 13

---
+++ js/myScript.js
@@ -0,0 +1,86 @@
+$('#subDiv').click(function () {
+
+   // var img1srcValue = $(this).attr('src');
+   $('#mainImage').attr('src', $('#subDiv').attr('src') );
+});
+$('#subDiv1').click(function () {
+
+    var img1srcValue = $(this).attr('src');
+    $('#mainImage').attr('src',img1srcValue);
+});
+$('#subDiv2').click(function () {
+
+    var img1srcValue = $(this).attr('src');
+    $('#mainImage').attr('src',img1srcValue);
+});
+$('#subDiv3').click(function () {
+
+    var img1srcValue = $(this).attr('src');
+    $('#mainImage').attr('src',img1srcValue);
+});
+
+
+// document.getElementById('btn').onclick = function () {
+//     var newHeadingElement = document.createElement('h1');
+//     var newParagraphElement = document.createElement('p');
+//
+//     newHeadingElement.innerHTML = 'This is a heading';
+//     newParagraphElement.innerHTML = 'This is a paragraph';
+//
+//     document.getElementById('div').appendChild(newHeadingElement);
+//     document.getElementById('div').appendChild(newParagraphElement);
+// };
+
+
+
+// document.getElementById('btn1').onclick = function () {
+//     var newHeadingElement = document.createAttribute('<input type="text">');
+//     var newParagraphElement = document.createAttribute('<input type="text">');
+//
+//     // newHeadingElement.innerHTML = 'This is a heading';
+//     // newParagraphElement.innerHTML = 'This is a paragraph';
+//     //
+//     // document.getElementById('div').appendChild(newHeadingElement);
+//     // document.getElementById('div').appendChild(newParagraphElement);
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $('#firstName').keyup(function () {
+//     var firstVal = $('#firstName').val();
+//
+//     $('#firstName2').val(firstVal);
+// });
+//
+// $('#lasttName').keyup(function () {
+//     var lastVal = $("#lasttName").val();
+//
+//     $('#lastName2').val(lastVal);
+//
+// });
+//
+// $('#lasttName').blur(function () {
+//     var firstVal = $('#firstName').val();
+//     var lastVal = $("#lasttName").val();
+//     $('#fullName').val(firstVal+' '+lastVal);
+// });
+
+
+
+
+
+
+
